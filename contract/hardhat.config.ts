689fd270ddae478af04a7e2d6a3c62da1ec7e3f9
Kostiantyn Baievskyi <konstantin.bay228@gmail.com>
2024-05-02 13:03:38 +0300

    Updated project: updated config file, cleaned up unnecessary files, added Solidity coverage.

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -5,13 +5,10 @@ import "@nomicfoundation/hardhat-verify";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const SEPOLIA_PRIVATE_KEY="YOUR_PRIVATE_KEY";
-const ALCHEMY_API_KEY="YOUR_ALCHEMY_API_KEY";
-const ETHERSCAN_API_KEY="YOUR_ETHERSCAN_API_KEY";
-// function privateKey() {
-//   return process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [];
-// }
-
+function privateKey() {
+  return process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [];
+}
+console.log(privateKey());
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
@@ -24,14 +21,14 @@ const config: HardhatUserConfig = {
       gasMultiplier: 1.2,
     },
     sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY]
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: privateKey()
     }
   },
   etherscan: {
     // Your API key for Etherscan
     // Obtain one at https://etherscan.io/
-    apiKey: `${ETHERSCAN_API_KEY}`
+    apiKey: `${process.env.ETHERSCAN_API_KEY}`
   },
   sourcify: {
     // Disabled by default
